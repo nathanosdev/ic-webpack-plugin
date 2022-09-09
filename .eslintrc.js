ed0c9e57aa99b3a4f45bd70859ca618aed648f20
Nathan Mc Grath <contact@nathanos.dev>
2022-09-09 21:22:01 +0200

    feat: setup repo

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:@typescript-eslint/strict',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['tsconfig.json'],
+    tsconfigRootDir: __dirname,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  plugins: ['@typescript-eslint'],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  rules: {
+    '@typescript-eslint/no-extraneous-class': [
+      'error',
+      { allowWithDecorator: true },
+    ],
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    '@typescript-eslint/explicit-member-accessibility': [
+      'error',
+      {
+        accessibility: 'explicit',
+        overrides: {
+          constructors: 'no-public',
+        },
+      },
+    ],
+    '@typescript-eslint/no-inferrable-types': 'error',
+  },
+};
---
+++ README.md
@@ -0,0 +1,17 @@
+# IC Canister Plugin
+
+## Overview
+
+A Webpack plugin to find your Internet Computer canister Ids and create environment variables for them.
+
+[![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](http://commitizen.github.io/cz-cli/)
+
+## Installation
+
+```shell
+$ npm i -D @solec/ic-canister-plugin
+```
+
+## Usage
+
+## Contributing
